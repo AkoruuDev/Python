a60748bb912ffc8ff1aa3f14b77b1949496be1ea
Allan Freitas (Akoruu) <akoruu.dev@gmail.com>
2022-05-23 14:54:30 -0300

    Create Conversor de temperatura.py
    Update excercise " Conversor de Temperatura"
    
    # Crie um programa que leia a temperatura em °C e converta mostrando na tela como °F

---
+++ Exercícios Curso em Video/Conversor de temperatura.py
@@ -0,0 +1,8 @@
+# ex14
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Crie um programa que leia a temperatura em °C e converta mostrando na tela como °F
+
+g = float(input('Digite qual a temperatura desejada em °C: '))
+f = ((9*g)/5)+32
+print(f'A temperatura de {g}°C corresponde à {f}°F')
