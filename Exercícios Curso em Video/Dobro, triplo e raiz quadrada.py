348d915082f11964e7c69733d1412817a0e471b9
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2022-05-23 19:29:58 -0300

    Create Dobro, triplo e raiz quadrada.py
    Update excercise "Dobro, Triplo e raiz quadrada"
    
    # Crie um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada

---
+++ Exercícios Curso em Video/Dobro, triplo e raiz quadrada.py
@@ -0,0 +1,10 @@
+# ex06
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Crie um algorítimo que leia um número e mostre seu dobro, triplo e raiz quadrada
+
+n = int(input('Digite um número: '))
+d = n*2
+t = n*3
+rq = n**(1/2)
+print(f'Você escolheu o número: {n}.\nSeu dobro é {d}\nSeu triplo é {t}\nE sua raiz quadrada é {rq}')
