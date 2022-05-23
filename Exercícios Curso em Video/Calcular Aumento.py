3e92fb86ef2517d9b6c3c47f9aeb99c1fc6c8749
Allan Freitas (Akoruu) <akoruu.dev@gmail.com>
2022-05-23 14:46:36 -0300

    Create Calcular Aumento.py
    Update excercise "Calcular Aumento"
    
    # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com a 15% de aumento

---
+++ Exercícios Curso em Video/Calcular Aumento.py
@@ -0,0 +1,7 @@
+# ex13
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com a 15% de almento
+
+salario = float(input('Digite seu salário atual: R$ '))
+print(f'Parabéns! você ganhou 15% de aumento e agora seu novo salário é de R$ {salario+(salario*0.15)}')
