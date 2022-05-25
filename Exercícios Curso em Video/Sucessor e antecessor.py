cda5b3868d150480479534bb846c72a059d4c24f
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2022-05-25 12:54:53 -0300

    Create Sucessor e antecessor.py
    Update excercise "Sucessor e Antecessor"
    
    # Crie um script que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor

---
+++ Exercícios Curso em Video/Sucessor e antecessor.py
@@ -0,0 +1,7 @@
+# ex05
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Crie um script que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um numero inteiro: '))
+print(f'O numero que você escolheu foi {n} e, de acordo com isso:\nSeu sucessor é {n+1}\nE seu antecessor é {n-1}')
