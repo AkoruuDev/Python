097afb708e322abd2c33e1f0df8571f15b75741d
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2022-05-25 12:18:52 -0300

    Create Mostrar pares.py
    Update exercise "Mostrar pares"
    
    # Faça um programa que mostre na tela todos os numeros pares que estão no intervalo entre 1 e 50

---
+++ Exercícios Curso em Video/Mostrar pares.py
@@ -0,0 +1,9 @@
+# ex47
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Faça um programa que mostre na tela todos os numeros pares que estão no intervalo entre 1 e 50
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
+        
