44dcb3dd7fcace10a3191c53b5b64ea02181711e
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2022-05-25 13:04:35 -0300

    Create Welcome.py
    Update excercise "Welcome"
    
    # Crie um script que leia o nome do usuário e imprima na tela uma mensagem de boas vindas

---
+++ Exercícios Curso em Video/Welcome.py
@@ -0,0 +1,7 @@
+# ex02
+# Exercício da aula de Python - Mundo um (Curso em Video)
+
+# Crie um script que leia o nome do usuário e imprima na tela uma mensagem de boas vindas
+
+nome = input('Digite seu nome: ')
+print(f'Olá {nome}! É um prazer te conhecer!')
